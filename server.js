3ac24a0e8bcc302dc1805553e532831ebcdf9a4a
Bryson <brysonttaylor@gmail.com>
2020-12-12 13:52:53 -0800

    Added in client config script

--- server.js
+++ server.js
@@ -20,8 +20,6 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
-// app.use(logger('dev'));
-// app.use(dbLogger(process.env.DB_URL, 'dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
