d80f54c5179bfa19d782aa1fe5642673f1612f0e
Bryson <brysonttaylor@gmail.com>
2020-12-14 17:17:27 -0800

    fixed the shit

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 // imports
 const express = require('express');
-const logger = require('morgan');
-const dbLogger = require('mongo-morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const usersRouter = require('./routes/users');
 const loginRouter = require('./routes/login');
 const messageRouter = require('./routes/messages');
+
+const path = require('path');
 
 
 // configs
@@ -19,6 +19,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -28,6 +30,7 @@ app.use('/login', loginRouter);
 app.use('/message', messageRouter);
 
 app.get('*', (req, res) => {
+    console.log(__dirname+'/client/build/index.html');
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
